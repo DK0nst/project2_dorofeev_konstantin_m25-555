4a1b37ca3f51918444b4f6779b44ac0a1b60533f
Konstantin Dorofeev <konstantin.s.dorofeev@gmail.com>
2026-01-31 14:11:55 +0300

    Добавлена функция welcome

---
+++ src/primitive_db/engine.py
@@ -0,0 +1,37 @@
+import prompt
+    
+def help():
+    print()
+    print(50*"=")
+    print("Справка")
+    print(50*"=")
+
+    print("exit - выйти из программы")
+    print("help - справочная информация")  
+
+    print(50*"=")
+
+def welcome():
+    
+    print("Первая попытка запустить проект!")
+    help()
+
+    while True:
+        try:
+            command = prompt.string("\nВведите команду: ").strip().lower()
+            
+            if command == "exit":
+                print("\nВыход из программы...")
+                break
+            elif command == "help":
+                help()
+            else:
+                # Пустая строка
+                continue
+                
+        except KeyboardInterrupt:
+            print("\nВыход из программы...")
+            break
+        except EOFError:
+            print("\nВыход из программы...")
+            break
